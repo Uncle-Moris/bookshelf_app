2728b92d14d084289adaa13d762323330b1efd71
maurycywoznica <mjw11@wp.pl>
2022-05-19 00:53:21 +0200

    __init__ books

--- core/books.py
+++ core/books.py
@@ -2,8 +2,19 @@ import connections
 import datetime
 
 class Book:
-    def __init__(self, title: str, author_id: int, status_id, published_at, categories_id):
-        pass
+    def __init__(self,
+                 title: str,
+                 author_id: int,
+                 status_id: int,
+                 published_at: datetime.date,
+                 categories_id: int):
+        self.title = title
+        self.authors_id = author_id
+        self.status_id = status_id
+        self.published_at = published_at
+        self.categories_id = categories_id
+
+
 
 class BooksManaging:
     pass
